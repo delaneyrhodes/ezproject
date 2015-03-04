68013af503ab9bec5d060a1dfa9a0c428b71ee9b
Delaney <delaney.rhodes@bobcats.gcsu.edu>
2015-03-03 23:19:49 -0500

    Create index.php
    prompts the user to login or create an account

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+echo<<<_END
+	<form action="main.php">
+    	<input type="submit" value="Log In">
+    </form>
+	<form action="create.php">
+    	<input type="submit" value="Create">
+    </form>
+_END
+?>
