305a204a0107e4ba8b0462521ffdbcc7198d5f49
Delaney <delaney.rhodes@bobcats.gcsu.edu>
2015-03-03 23:20:14 -0500

    Create dbConnect.php
    connects to the database

---
+++ dbConnect.php
@@ -0,0 +1,10 @@
+<?php
+	$db_host = "198.71.225.53:3306 ";
+	$db_user = "newuser";
+	$db_pass = "NewUser2015";
+	$db_name = "ezdatabase";
+
+	$db_connect = mysql_connect($db_host, $db_user, $db_pass) 
+		or die ("Could not connect to MySQL");
+	$db_select = @mysql_select_db($db_name) or die ("Could not connect to database");
+?>
